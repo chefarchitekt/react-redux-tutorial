297b941b9f9ff0620cd36cf9e1acd3f7c83c6cf6
chefarchitekt <chefarchitekt@gmail.com>
2017-09-13 23:11:12 +0800

    add babel loaders for webpack conpiler and add babel-preset for react

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,7 +2,19 @@ import path from 'path';
 
 export default {
     entry: path.join(__dirname, '/client/index.js'),
-    output:{
+    output: {
         path: '/'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                include: path.join(__dirname, 'client'),
+                loaders: [ 'babel']
+            }
+        ]
+    },
+    resolve: {
+        extentions: [ '', '.js']
     }
 }
