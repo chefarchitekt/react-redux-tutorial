45130589e77a0a53180f69b4e49d0e1fdf96077b
chefarchitekt <chefarchitekt@gmail.com>
2017-09-14 18:32:50 +0800

    change top of component from functional component to class component

--- client/components/App.js
+++ client/components/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
+/*
 export default () => {
     return (
-        <h1>Hello from React</h1>
+        <h1>Hello from React!!!</h1>
     );
 }
+*/
+
+//need to use a class component for the top of hirerarchy instead of functional component
+//for hot loading to work
+class App extends Component {
+    render() {
+        return (
+            <h1>Hello from React!!!**#@##II</h1>
+        );
+    }
+}
+
+export default App;
