ddde6a63c0b78f29dec2b923ff80ddac4578301c
chefarchitekt <chefarchitekt@gmail.com>
2017-09-20 14:05:18 +0800

    fix validator

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,49 @@
 import express from 'express';
+import validator from 'validator';
+import isEmpty from 'lodash/isEmpty';
 
 const router = express.Router();
 
+const validateInput = (data) => {
+    const errors = {};
+
+    if (validator.isEmpty(data.username)) {
+        errors.username = 'This field is required';
+    }
+
+    if (validator.isEmpty(data.email)) {
+        errors.email = 'This field is required';
+    }
+
+    if (validator.isEmpty(data.password)) {
+        errors.password = 'This field is required';
+    }
+
+    if (validator.isEmpty(data.passwordConfirmation)) {
+        errors.passwordConfirmation = 'This field is required';
+    }
+
+    if (!validator.equals(data.password, data.passwordConfirmation)) {
+        errors.passwordConfirmation = 'password must match';
+    }
+
+    if (validator.isEmpty(data.timezone)) {
+        errors.timezone = 'This field is required';
+    }
+
+    return {
+        errors, //shorthand of errors: errors
+        isValid: isEmpty(errors)
+    };
+};
+
 router.post('/', (req, res) => {
+    const { errors, isValid } = validateInput(req.body); //return isValida or errors
+
+    if (!isValid) {
+        res.status(400).json(errors);
+    }
+
     console.log(req.body);
 });
 
